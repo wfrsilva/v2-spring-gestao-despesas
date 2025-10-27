e7ab97b50f17d4519c112cb59f84f59281b8f7b7
wfrsilva <wfrsilva@gmail.com>
2025-10-27 19:17:11 -0300

    Concluido ErrorMessage.java
     Changes to be committed:
    	modified:   src/main/java/dev/wfrsilva/gestao_despesas/custom_message/ErrorMessage.java

--- src/main/java/dev/wfrsilva/gestao_despesas/custom_message/ErrorMessage.java
+++ src/main/java/dev/wfrsilva/gestao_despesas/custom_message/ErrorMessage.java
@@ -1,5 +1,34 @@
 package dev.wfrsilva.gestao_despesas.custom_message;
 
 public class ErrorMessage {
+
+    private String message;
+    private String type;
+
+    public ErrorMessage(String message, String type)
+    {
+        this.message = message;
+        this.type = type;
+    }//construcotr all args
+
+    public String getMessage()
+    {
+        return message;
+    }//getMessage
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }//setMessage
+
+    public String getType()
+    {
+        return type;
+    }//getType
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }//setType
     
-}
+}//ErrorMessage
