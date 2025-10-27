e6de316903b35fa7186ab73a22c68bca3c05387b
wfrsilva <wfrsilva@gmail.com>
2025-10-27 19:01:49 -0300

    Iniciando DespesaRepository.java
     Changes to be committed:
    	modified:   src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
    	new file:   src/main/java/dev/wfrsilva/gestao_despesas/repository/DespesaRepository.java
    	modified:   src/main/resources/application.properties

--- src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
+++ src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
@@ -4,20 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import dev.wfrsilva.gestao_despesas.entity.Despesa;
+import dev.wfrsilva.gestao_despesas.useCase.CadastroDespesaUseCase;
 
 @RequestMapping("/gestao")
 @RestController
 public class GestaoDespesaController {
     
     @Autowired
-    CadastroDespesaUseCase cadastroDespesaUC
+    CadastroDespesaUseCase cadastroDespesaUC;
 
     @Autowired
-    BuscarDespesaUseCase buscarDespesaUC
+    BuscarDespesaUseCase buscarDespesaUC;
 
 
     @PostMapping("/create") 
---
+++ src/main/java/dev/wfrsilva/gestao_despesas/repository/DespesaRepository.java
@@ -0,0 +1,5 @@
+package dev.wfrsilva.gestao_despesas.repository;
+
+public class DespesaRepository {
+    
+}
