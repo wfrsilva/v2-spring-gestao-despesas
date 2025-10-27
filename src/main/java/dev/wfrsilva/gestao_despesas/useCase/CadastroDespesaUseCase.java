7c656958e1d863577b26a497795a1804d78a7a83
wfrsilva <wfrsilva@gmail.com>
2025-10-27 18:50:24 -0300

    Concluido CadastroDespesaUseCase.java
     Changes to be committed:
    	modified:   src/main/java/dev/wfrsilva/gestao_despesas/useCase/CadastroDespesaUseCase.java

--- src/main/java/dev/wfrsilva/gestao_despesas/useCase/CadastroDespesaUseCase.java
+++ src/main/java/dev/wfrsilva/gestao_despesas/useCase/CadastroDespesaUseCase.java
@@ -1,5 +1,28 @@
 package dev.wfrsilva.gestao_despesas.useCase;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.wfrsilva.gestao_despesas.entity.Despesa;
+
+@Service
 public class CadastroDespesaUseCase {
-    
-}
+    //SOLID Single Responsability Principe
+
+    @Autowired
+    private DespesaRepository despesaRepository;
+
+    public Despesa execute(Despesa despesa)
+    {
+        
+        if(despesa.getCategoria() == null || despesa.getData() == null || despesa.getDescricao() == null || despesa.getEmail() == null)
+        {
+            throw new IllegalArgumentException("Preencher todos os campos");
+        }//if
+
+        despesa = despesaRepository.save(despesa);
+        return despesa;
+
+    }//execute
+
+}//CadastroDespesaUseCase
