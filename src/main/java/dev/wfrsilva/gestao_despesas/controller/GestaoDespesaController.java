d1c2ab1ffe81fdb40e71b2f98c969e1d7e423927
wfrsilva <wfrsilva@gmail.com>
2025-10-27 18:20:24 -0300

    Concluido GestaoDespesaController.java
     Changes to be committed:
    	modified:   src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java

--- src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
+++ src/main/java/dev/wfrsilva/gestao_despesas/controller/GestaoDespesaController.java
@@ -1,5 +1,46 @@
 package dev.wfrsilva.gestao_despesas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/gestao")
+@RestController
 public class GestaoDespesaController {
     
-}
+    @Autowired
+    CadastroDespesaUseCase cadastroDespesaUC
+
+    @Autowired
+    BuscarDespesaUseCase buscarDespesaUC
+
+
+    @PostMapping("/create") 
+    public ResponseEntity<?> create(@RequestBody Despesa despesa)
+    {
+
+        try
+        {   
+            var result = cadastroDespesaUC.execute(despesa);
+            return ResponseEntity.ok(result);
+        }//try
+        catch(IllegalArgumentException e)
+        {
+            var errorMessage = new ErrorMessage(e.getMessage(), "INVALID_PARAMS")
+            return ResponseEntity.status(400).body(errorMessage);
+        }//catch
+
+    }//create
+
+    @GetMapping("/{email}")
+    public List <Despesa> findByEmailAndDate(@PathVariable String email, @RequestParam(required = false) LocalDate data)
+    {
+        return buscarDespesaUC.execute(email, data);
+    }//findByEmailAndDate
+
+}//GestaoDespesaController
